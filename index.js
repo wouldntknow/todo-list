cb640c0772af2149d3e43b38e32254b3d0e524d8
wouldntknow <134201344+wouldntknow@users.noreply.github.com>
2024-08-15 08:34:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let i = true;
+while (i) {
+    let Addtasks = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "Add todos",
+        },
+        {
+            name: "Addmore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        },
+    ]);
+    todos.push(Addtasks.todo);
+    i = Addtasks.Addmore;
+    console.log(todos);
+}
